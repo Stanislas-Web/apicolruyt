b8d69becd5384204d6ec07f3108d9a734a07946a
Stanislas-Web <stamislasmakengo1@gmail.com>
2023-04-12 14:46:51 +0100

    probleme connection

--- routers/role.router.js
+++ routers/role.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { createRole, getAllRole } = require('../controllers/role.controller');
 // const { isLoggedIn } = require("../middleware");
 
-router.route('/roles').get(createRole);
-router.route('/roles').post(getAllRole);
+router.route('/roles').get(getAllRole);
+router.route('/roles').post(createRole);
 
 module.exports = router; 
